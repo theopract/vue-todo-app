b13723e7b5fc66700611b042501f8ec189a20014
Pavel Melnik <theopract@gmail.com>
2018-07-22 18:40:26 +0300

    Разбил на компоненты todo_list и todo_list_item

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 let nextTodoId = 1;
 
-var app = new Vue ({
-  el: "#app",
-  data: {
-    value: "",
-    todos: [
-      {
-        id: nextTodoId++,
-        text: 'Learn Vue'
-      },
-      {
-        id: nextTodoId++,
-        text: 'Learn about single-file components'
-      },
-      {
-        id: nextTodoId++,
-        text: 'Fall in love'
-      }
-    ]
+Vue.component('todo-list', {
+  template: `
+  <div>
+    <input type="text" class="input" v-model="value" @keydown.enter="addTodo">
+    <ul v-if="todos.length">
+      <todo_list_item 
+          v-for="todo in todos"
+          :key="todo.id"
+          :todo="todo"
+          @remove="removeTodo">
+        </todo_list_item>
+    </ul>
+  </div>
+  `,
+  data: function () {
+    return {
+      value: "",
+      todos: [
+        {
+          id: nextTodoId++,
+          text: 'Learn Vue'
+        },
+        {
+          id: nextTodoId++,
+          text: 'Learn about single-file components'
+        },
+        {
+          id: nextTodoId++,
+          text: 'Fall in love'
+        }
+      ]
+    }
   },
   methods: {
     addTodo () {
@@ -36,4 +50,26 @@ var app = new Vue ({
       })
     }
   }
+})
+
+
+Vue.component("todo_list_item", {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  template: `
+    <li>
+      {{ todo.text }}
+      <button @click="$emit('remove', todo.id)">
+        X
+      </button>
+    </li>
+  `
+})
+
+var app = new Vue ({
+  el: "#app",
 })
